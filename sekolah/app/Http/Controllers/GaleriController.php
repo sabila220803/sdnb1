1d0a6e22aca79f17e3db559f93730e29b10debc0
sabila220803 <sabilarealme2283@gmail.com>
2025-05-29 20:53:07 +0700

    feat(galeri): enhance photo gallery with pagination and dynamic content loading

--- sekolah/app/Http/Controllers/GaleriController.php
+++ sekolah/app/Http/Controllers/GaleriController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Galeri as Gallery;
 use Illuminate\Http\Request;
 
 class GaleriController extends Controller
 {
     public function foto()
     {
-        return view('galeri.foto');
+        $galleries = Gallery::orderBy('created_at', 'DESC')->paginate(9); // Paginate comes last
+        return view('galeri.foto', compact('galleries'));
     }
 
     public function video()
